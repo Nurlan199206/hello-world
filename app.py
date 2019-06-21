395f398fb95373ae9d89491b92bc7715987d4738
root <user_1771@mail.ru>
2019-06-21 17:05:31 +0000

    v2.0.0

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello, World v1.0!</h3>"
+    html = "<h3>Hello, World v2.0.0!</h3>"
     return html
 
 if __name__ == "__main__":
